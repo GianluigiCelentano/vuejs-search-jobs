0f20da5a3de466827388937163bc323d1b1dcdfe
GianluigiCelentano <cgianluigi96@gmail.com>
2021-07-06 12:28:47 +0200

    Web App Search Jobs

---
+++ script.js
@@ -0,0 +1,63 @@
+const app = new Vue ({
+    el: '#app',
+    data: {
+        jobs: [
+            {
+              id: 1,
+              company: 'Perferendis',
+              position: 'Developer',
+              description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+              created_at: '5/22/2021',
+              logo: 'logo.jpg',
+              city: 'Roma',
+              contract: 'Full Time'
+            },  
+            {
+              id: 2,
+              company: 'Perferendis',
+              position: 'Developer',
+              description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+              created_at: '5/22/2021',
+              logo: 'logo.jpg',
+              city: 'Roma',
+              contract: 'Full Time'
+            },  
+            {
+              id: 3,
+              company: 'Perferendis',
+              position: 'Developer',
+              description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+              created_at: '5/22/2021',
+              logo: 'logo.jpg',
+              city: 'Roma',
+              contract: 'Full Time'
+            },  
+            {
+              id: 4,
+              company: 'Perferendis',
+              position: 'Developer',
+              description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+              created_at: '5/22/2021',
+              logo: 'logo.jpg',
+              city: 'Roma',
+              contract: 'Full Time'
+            },  
+            {
+              id: 5,
+              company: 'Perferendis',
+              position: 'Developer',
+              description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+              created_at: '5/22/2021',
+              logo: 'logo.jpg',
+              city: 'Roma',
+              contract: 'Full Time'
+            },  
+        ],
+        starred: [1, 2, 3],
+        applied: [4, 5]
+
+    },
+    methods: {
+       
+    }
+})
